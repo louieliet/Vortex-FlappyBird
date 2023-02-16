8f61fd4558bbf07670f3da8e7f0938fb82dfa626
louieliet <101071655+louieliet@users.noreply.github.com>
2023-02-16 13:39:47 -0600

    updated

--- FlappyBird/Assets/Scripts/PipeMovement.cs
+++ FlappyBird/Assets/Scripts/PipeMovement.cs
@@ -5,9 +5,16 @@ using UnityEngine;
 public class PipeMovement : MonoBehaviour
 {
     public float speed = 5.0f;
+    public float moveSpeed = 5;
+    public float deadZone = -10;
 
     void Update()
     {
         transform.position += new Vector3(-1,0,0) * Time.deltaTime;
+
+        if(transform.position.x < -8){
+            Destroy(gameObject);
+        }
+
     }
 }
--- FlappyBird/Assets/Scripts/PipeSpawner.cs
+++ FlappyBird/Assets/Scripts/PipeSpawner.cs
@@ -7,6 +7,7 @@ public class PipeSpawner : MonoBehaviour
     public GameObject pipePrefab;
     public float spawnRate = 1f;
     private float timer = 0f;
+    public float heightOffset = 10;
 
     private void Start()
     {
@@ -15,16 +16,21 @@ public class PipeSpawner : MonoBehaviour
     
     void Update()
     {
-        if(timer > spawnRate){
-            timer = 0f;
-            spawnPipe();
+        if (timer < spawnRate)
+        {
+            timer = timer + Time.deltaTime;
         }
-        else{
-            timer += Time.deltaTime;
+        else
+        {
+            spawnPipe();
+            timer = 0;
         }
     }
 
     void spawnPipe(){
-        Instantiate(pipePrefab, new Vector3(10,0,0), pipePrefab.transform.rotation);
+
+        float lowestPoint = transform.position.y - heightOffset;
+        float highestPoint = transform.position.y + heightOffset;
+        Instantiate(pipePrefab, new Vector3(transform.position.x ,Random.Range(lowestPoint,highestPoint),0), pipePrefab.transform.rotation);
     }
 }
